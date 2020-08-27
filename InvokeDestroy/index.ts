be1b388e8b9cde32db1a492aee4030c90e31060b
Logan Ballard <loganballard@gmail.com>
2020-08-27 19:04:38 +0000

    Merged PR 5: asdfavds

---
+++ InvokeDestroy/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+
+    if (name) {
+        console.log(JSON.stringify(req, null, 4));
+        context.res = {
+            body: req.body
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
+
+export default httpTrigger;
