8e08da9ae8ec0ca098c8787ac5b356be64a89116
loganballard <loganballard@gmail.com>
2020-08-27 18:52:55 -0700

    adding get pipeline id

--- InvokeDestroy/pipeline_info.ts
+++ InvokeDestroy/pipeline_info.ts
@@ -1,6 +1,7 @@
 import secrets from '../secrets.json';
 import fetch from 'node-fetch';
 import { encode } from 'js-base64';
+import { none } from 'fp-ts/lib/Option';
 
 const adoOrg = "loganballard0423";
 const adoProj = "testing";
@@ -19,7 +20,7 @@ async function get_access_token(accessTokenUrl: string, tokenBody: string): Prom
         .catch();
 }
 
-async function list_pipelines(pipelineUrl: string): Promise<Object> {
+async function list_pipelines(pipelineUrl: string): Promise<Array<Object>> {
     return await fetch(pipelineUrl, {
         method: 'get',
         headers: {
@@ -28,18 +29,18 @@ async function list_pipelines(pipelineUrl: string): Promise<Object> {
         }
     })
         .then(res => res.json())
-        .then(data => data)
+        .then(data => data['value'])
 }
 
-// get_access_token(tokenUrl, tokenAuthBody)
-//     .then(tok => {
-//         list_pipelines(pipelineListUrl)
-//             .then(data => console.log(data));
-//     })
+async function get_pipeline_id_by_name(name: string): Promise<number> {
+    let pipelines = await list_pipelines(pipelineListUrl)
+        .then(data => data);
+    for (var i = 0; i < pipelines.length; i++) {
+        if (pipelines[i]['name'] === name) {
+            return pipelines[i]['id'];
+        }
+    }
+    return -1;
+}
 
-list_pipelines(pipelineListUrl)
-    .then(data => {
-        data['value'].forEach(pipeline => {
-            console.log(`pipeline name: ${pipeline['name']}`)
-        });
-    });
+get_pipeline_id_by_name('testing-pipeline-vars').then(data => console.log(data));
